923a8f78be0bec569352738d584f12dc06bc986e
DevepMau <oshiettecs@gmail.com>
2024-06-02 16:29:25 -0300

    Se implemento la funcion addToCart a Item.jsx

--- src/pages/Item.jsx
+++ src/pages/Item.jsx
@@ -1,12 +1,34 @@
-import React from 'react'
+import React, { useContext } from 'react'
+import { CartContext } from '../contexts/ShoppingCartContext';
 
 export default function Item({ id, nombre, descripción, precio, categoría, imagen, stock }) {
+  
+  const [cart, setCart] = useContext(CartContext);
+
+  const addToCart = (item) => {
+    // Verificar si el item ya está en el carrito
+    const existingItem = cart.find(cartItem => cartItem.id === item.id);
+    if (existingItem) {
+      // Si ya existe, actualizamos la cantidad
+      const updatedCart = cart.map(cartItem => {
+        if (cartItem.id === item.id) {
+          return { ...cartItem, cantidad: cartItem.cantidad + 1 };
+        }
+        return cartItem;
+      });
+      setCart(updatedCart);
+    } else {
+      // Si no existe, agregamos el item al carrito con cantidad 1
+      setCart([...cart, { ...item, cantidad: 1 }]);
+    }
+  }
+  
   return (
     <div>
       <div>{nombre}</div>
       <img src={imagen} width={300} height={300} alt={nombre} />
       <div>${precio}</div>
-      <button>
+      <button onClick={() => addToCart({ id, nombre, precio, categoría, imagen })} >
         buy
       </button>
     </div>
